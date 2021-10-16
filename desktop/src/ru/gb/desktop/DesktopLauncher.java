c66bc08a43e5975821034d60ccbc092ee2ef3537
Ps1h0 <platonov.liceum@mail.ru>
2021-10-17 00:14:11 +0300

    Initial commit

---
+++ desktop/src/ru/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.gb.Ps1h0Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ps1h0Game(), config);
+	}
+}
